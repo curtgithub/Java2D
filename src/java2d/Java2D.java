d1991ff4283b740fe03f62a756349a25ca630222
owner <owner@Owner01>
2021-02-06 00:58:03 -0500

    Added DrawingArbitraryShapes. Call from Java2D.main

--- src/java2d/Java2D.java
+++ src/java2d/Java2D.java
@@ -5,18 +5,21 @@
  */
 package java2d;
 
+import java2d.examples.DrawingArbitraryShapes;
+
 /**
  *
  * @author owner
  */
 public class Java2D {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Java2D.main run...\n");
-    }
-    
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      System.out.println("java2d.Java2D.main run...\n");
+      System.out.println("java2d.examples.DrawingArbitraryShames.main ...\n");
+      DrawingArbitraryShapes.main(null);
+   }
+   
 }
