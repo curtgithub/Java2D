9af9898da30a52bf11c64977413af105d6a4c79e
owner <owner@Owner01>
2021-06-05 15:31:45 -0400

    Added.

---
+++ src/tutorialspoint/gui/SwingControlDemo.java
@@ -0,0 +1,80 @@
+/*
+ * Taken from:
+ * https://www.tutorialspoint.com/swing/swing_imageicon.htm
+ */
+package tutorialspoint.gui;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author owner
+ */
+public class SwingControlDemo {
+
+   private JFrame mainFrame;
+   private JLabel headerLabel;
+   private JLabel statusLabel;
+   private JPanel controlPanel;
+
+   public SwingControlDemo() {
+      prepareGUI();
+   }
+
+   public static void main(String[] args) {
+      SwingControlDemo swingControlDemo = new SwingControlDemo();
+      swingControlDemo.showImageIconDemo();
+   }
+
+   private void prepareGUI() {
+      mainFrame = new JFrame("Java Swing Examples");
+      mainFrame.setSize(400, 400);
+      mainFrame.setLayout(new GridLayout(3, 1));
+
+      mainFrame.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent windowEvent) {
+            System.exit(0);
+         }
+      });
+      headerLabel = new JLabel("", JLabel.CENTER);
+      statusLabel = new JLabel("", JLabel.CENTER);
+      statusLabel.setSize(350, 100);
+
+      controlPanel = new JPanel();
+      controlPanel.setLayout(new FlowLayout());
+
+      mainFrame.add(headerLabel);
+      mainFrame.add(controlPanel);
+      mainFrame.add(statusLabel);
+      mainFrame.setVisible(true);
+   }
+
+   // Returns an ImageIcon, or null if the path was invalid. 
+   private static ImageIcon createImageIcon(String path,
+           String description) {
+      java.net.URL imgURL = SwingControlDemo.class.getResource(path);
+
+      if (imgURL != null) {
+         return new ImageIcon(imgURL, description);
+      } else {
+         System.err.println("Couldn't find file: " + path);
+         return null;
+      }
+   }
+
+   private void showImageIconDemo() {
+      headerLabel.setText("Control in action: ImageIcon");
+      ImageIcon icon = createImageIcon("/resources/java_icon.png", "Java");
+
+      JLabel commentlabel = new JLabel("", icon, JLabel.CENTER);
+      controlPanel.add(commentlabel);
+      mainFrame.setVisible(true);
+   }
+}
